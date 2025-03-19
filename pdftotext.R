a9feff4d12ee9bc794c984ce09480c554d92366f
Sara Ludwick <saraludwick@saraludwick.local>
2025-03-19 09:29:10 -0700

    pdf to text

--- pdftotext.R
+++ pdftotext.R
@@ -6,30 +6,34 @@
 CLOBBER <- FALSE  # Set this to TRUE if you want to overwrite existing txt files
 
 packs <- c("pdftools", "data.table", "tesseract")
-### check if package already installed
 need <- packs[!packs %in% installed.packages()[,'Package']]
-### install things that arent' installed yet
 install.packages(need)
-### load packages
 sapply(packs,library,character.only = T)
 
-# code here will get pdf files
-# then will process to .txt
-# and save to new txt directory
-## figure out symbolic link to tijuanabox
 
-# here is code from salinas:
-for (file in pdfiles) {
-  txt_file_path <- file.path(txt_file_directory, sub("\\.pdf$", ".txt", file))
+### here will need to figure out what to do with docs that are in chapters vs complete plans
+
+# directories to raw pdfs and folder to store converted txt files
+pdf_dir <- "tijuanabox/raw_data/plan_pdfs"
+txt_dir <- "tijuanabox/int_data/plan_txts"
+if (!dir.exists(txt_dir)) {
+  dir.create(txt_dir)
+}
+
+pdfs <- list.files(pdf_dir) 
+
+# here is code adapted from salinas:
+for (file in pdfs) {
+  txt_file_path <- file.path(txt_dir, sub("\\.pdf$", ".txt", file))
   if (file.exists(txt_file_path) && !CLOBBER) {
     next
   }
   tryCatch({
     #we suppressmessages according to documentation of pdftools since the function is extremely verbose
-    text <- suppressMessages(pdf_text(file.path(pdf_file_directory, file)))
+    text <- suppressMessages(pdf_text(file.path(pdf_dir, file)))
     # If the text is null or empty, use OCR to get the text
     if (is.null(text) || !any(nchar(unlist(text)) > 0)) {
-      text <- suppressMessages(pdf_ocr_text(file.path(pdf_file_directory, file)))
+      text <- suppressMessages(pdf_ocr_text(file.path(pdf_dir, file)))
     }
     # If the text is not null or empty, write it to the txt file    
     if (!is.null(text)) {
